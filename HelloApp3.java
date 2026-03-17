911f5e982f86496fb65f5a99b280612e89638ab2
Ayush Agarwal <ayushagarwal@Ayushs-MacBook-Air-1037.local>
2026-03-17 08:53:28 +0530

    UC3,4

---
+++ HelloApp3.java
@@ -0,0 +1,12 @@
+public class HelloApp3 {
+   
+public class HelloApp {
+
+    public static void main(String[] args) {
+
+        String name = (args.length > 0) ? args[0] : "World";
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
+}
